a3d52936af63d13bbebc5c1074a87d311a92d9a1
Denys Horopashnyi <gdi1991@gmail.com>
2020-03-16 22:25:58 +0200

    finish toggleWarningState()

--- js/app.js
+++ js/app.js
@@ -4,7 +4,13 @@ const USER_ROW_TEMPLATE = document.getElementById('template').innerHTML;
 const URL = 'json/json.txt';
 
 const userTableBody = document.getElementsByClassName('table__body')[0];
-const userTableRow = document.getElementsByClassName('template-row');
+const userTableRow = userTableBody.getElementsByTagName('tr');
+const userTableFooter = document.getElementsByClassName('table__footer')[0];
+const userTemplateRow = document.getElementsByClassName('template-row');
+
+const selectDay = document.getElementById('select--day');
+const selectMonth = document.getElementById('select--month');
+const dateOfBirth = document.getElementsByClassName('template__dob');
 
 init();
 
@@ -14,39 +20,48 @@ function init() {
 
 function fetchUsers() {
     return fetch(URL)
-        .then((resp) => resp.json())
+        .then(resp => resp.json())
         .then(renderUsers)
-        .then(filterData)
+        .then(addEvents);
 }
 
 function renderUsers(data) {
-    userTableBody.innerHTML = data.map((user) => {
-        return USER_ROW_TEMPLATE
-            .replace('{{firstname}}', user.firstname)
-            .replace('{{lastname}}', user.lastname)
-            .replace('{{email}}', user.email)
-            .replace('{{phonenumber}}', user.phonenumber)
-            .replace('{{dob}}', user.birthday_contact)
-            .replace('{{company}}', user.company)
-    }).join('\n');
+    userTableBody.innerHTML = data
+        .map(user => {
+            return USER_ROW_TEMPLATE
+                .replace('{{firstname}}', user.firstname)
+                .replace('{{lastname}}', user.lastname)
+                .replace('{{email}}', user.email)
+                .replace('{{phonenumber}}', user.phonenumber)
+                .replace('{{dob}}', user.birthday_contact)
+                .replace('{{company}}', user.company);
+        })
+        .join('\n');
 }
 
-const selectDay = document.getElementById("selectDay");
-const selectMonth = document.getElementById("selectMonth");
-const dateOfBirth = document.getElementsByClassName('template__dob');
-const tr = userTableBody.getElementsByTagName('tr');
-// 
-selectMonth.addEventListener('change', filterData);
-selectDay.addEventListener('change', filterData);
+function addEvents() {
+    selectMonth.addEventListener('change', filterData);
+    selectDay.addEventListener('change', filterData);
+}
 
 function filterData() {
-
-    for (let i = 0; i < tr.length; i++) {
-        if (dateOfBirth[i].innerText.split('-')[2] == selectDay.value && dateOfBirth[i].innerText.split('-')[1] == selectMonth.value) {
-            tr[i].style.display = "";
+    for (let i = 0; i < userTableRow.length; i++) {
+        if (
+            dateOfBirth[i].innerText.split('-')[2] == selectDay.value &&
+            dateOfBirth[i].innerText.split('-')[1] == selectMonth.value
+        ) {
+            userTableRow[i].style.display = '';
+            userTableRow[i].classList.add('see');
         } else {
-            console.log('99');
-            tr[i].style.display = "none";
+            userTableRow[i].classList.remove('see');
+            userTableRow[i].style.display = 'none';
         }
     }
+    toggleWarningState();
+}
+
+function toggleWarningState() {
+    let userArr = Array.from(userTableRow);
+    const elemFound = userArr.find(e => e.classList.contains('see'));
+    elemFound ? userTableFooter.classList.add('hide') : userTableFooter.classList.remove('hide');
 }
